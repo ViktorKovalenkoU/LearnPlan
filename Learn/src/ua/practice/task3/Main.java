9f12e604c93a462f17b732a27911cbfc411f8b17
ViktorKovalenkoU <132162098+ViktorKovalenkoU@users.noreply.github.com>
2024-06-18 21:57:14 +0300

    Task3 (array)

--- Learn/src/ua/practice/task3/Main.java
+++ Learn/src/ua/practice/task3/Main.java
@@ -1,16 +1,24 @@
 package ua.practice.task3;
 
 import java.util.Random;
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args){
         Random random = new Random();
+        Scanner sc = new Scanner(System.in);
 
         int[] num = new int[10];
         int max = Integer.MIN_VALUE;
+
+        System.out.println("Number range from?");
+        int a = sc.nextInt();
+        System.out.println("To:");
+        int b = sc.nextInt();
+
         for (int i = 0; i < num.length; i++) {
-            num[i] = random.nextInt(0, 100);
+            num[i] = random.nextInt(a, b);
             System.out.print(num[i] + " ");
 
             if(num[i] > max){
